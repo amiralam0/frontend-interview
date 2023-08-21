e9a05c4cde87d40edcec511cb9aae5ad01f58fa7
amiralam0 <sharpprogrammer2018@gmail.com>
2023-08-21 16:02:32 +0530

    init

---
+++ basic/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <form>
+        <label>Email</label>
+        <input name='email'/><br/>
+        <label>username</label>
+        <input name='username'/><br/>
+        <label>password</label>
+        <input/><br/>
+        <label>confirm password</label>
+        <input/><br/>
+        <label>phone</label>
+        <input/><br/>
+        <label>Date</label>
+        <input type='date'/><br/>
+        <br /><br />
+        <button type='submit'>Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default App
